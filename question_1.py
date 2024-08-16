8c562dad20f3373e6c1a4fe739effddfff4a1222
caljohns611 <caljohns18.5@gmail.com>
2024-08-16 00:32:34 -0500

    First commit

---
+++ question_1.py
@@ -0,0 +1,12 @@
+# question 1 task 1
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+grades.reverse()
+print(grades)
+
+#question 1 task 2 
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+average_grade = "84.6"
+print(average_grade)
---
+++ question_3.py
@@ -0,0 +1,12 @@
+#question 3 task 1 
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+segment = temperatures[7:14]
+print(segment)
+
+#question 3 task 2 
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+second_segment = temperatures[23:30]
+both_segment = segment + second_segment
+print(both_segment)
